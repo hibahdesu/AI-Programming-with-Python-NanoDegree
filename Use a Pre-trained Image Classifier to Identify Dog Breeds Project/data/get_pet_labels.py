9ac9c5f7da7298e1b2998236aafc2e25aaad631e
HIBAH SINDI <92212557+hibahdesu@users.noreply.github.com>
2024-06-22 17:51:15 +0300

    Add files via upload

--- Use a Pre-trained Image Classifier to Identify Dog Breeds Project/data/get_pet_labels.py
+++ Use a Pre-trained Image Classifier to Identify Dog Breeds Project/data/get_pet_labels.py
@@ -2,9 +2,9 @@
 # -*- coding: utf-8 -*-
 # */AIPND-revision/intropyproject-classify-pet-images/get_pet_labels.py
 #                                                                             
-# PROGRAMMER: 
-# DATE CREATED:                                  
-# REVISED DATE: 
+# PROGRAMMER: HIBAH SINDI
+# DATE CREATED: 22/06/2024                                 
+# REVISED DATE: 22/06/2024
 # PURPOSE: Create the function get_pet_labels that creates the pet labels from 
 #          the image's filename. This function inputs: 
 #           - The Image Folder as image_dir within get_pet_labels function and 
@@ -41,7 +41,64 @@ def get_pet_labels(image_dir):
          index 0 = pet image label (string)
     """
 
+    ## First, I will create the filenames of the images files with the os library (listdir)
+    images_names = listdir(image_dir)
+    # print("\nPrints 10 filenames from folder pet_images/")
+    # for idx in range(0, 10, 1):
+    #   print("{:2d} file: {:>25}".format(idx + 1, images_names[idx]) )
+
+
+    ## Next, creating an empty dictionary with the name results_dic by using {} which means an empty dictionary
+    results_dic = {}
+
+    ## Creating a for loop to loop through each name of images from the file
     
+    for i in range(0, len(images_names), 1):
+      
+      if images_names[i][0] != '.':
+        # print(images_names[i])
+        # print(images_names[i][0])
+        ## Making the names lower and split each word from _
+        names_formated = images_names[i].lower().split('_')
+        # print(names_formated)
+
+        #Creating an empty variable for pets' names
+        pets = ""
+
+        #Looping through each name that I have just formated
+        for name in names_formated:
+          # print(name)
+
+          #Getting only the names without numbers and the .jpg formate of the images
+          if name.isalpha():
+            # print(name)
+
+            pets += name + ' '
+            # print(pets)
+          pets = pets.strip()
+          # print(pets)
+
+          if images_names[i] not in results_dic:
+            results_dic[images_names[i]] = pets
+            # print(results_dic[images_names[i]])
+            # print(len(results_dic))
+            # print(results_dic)
+          
+          else: 
+            print("** Warning: Key=", images_names[i],  "there is the same value in =", results_dic[images_names[i]], "**")
+            print('Please try again later.')
+
+
+
+
+
+      
+
+
     # Replace None with the results_dic dictionary that you created with this
     # function
     return results_dic
+
+
+##This is just for checking the code     
+get_pet_labels("pet_images/")
