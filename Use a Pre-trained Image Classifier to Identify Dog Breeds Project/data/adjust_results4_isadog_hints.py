dd2054dd91e098ea21bb0ef163795e761fc9ae12
HIBAH SINDI <92212557+hibahdesu@users.noreply.github.com>
2024-06-23 21:27:47 +0300

    Add files via upload

--- Use a Pre-trained Image Classifier to Identify Dog Breeds Project/data/adjust_results4_isadog_hints.py
+++ Use a Pre-trained Image Classifier to Identify Dog Breeds Project/data/adjust_results4_isadog_hints.py
@@ -2,9 +2,9 @@
 # -*- coding: utf-8 -*-
 # */AIPND-revision/intropyproject-classify-pet-images/adjust_results4_isadog_hints.py
 #                                                                             
-# PROGRAMMER: 
-# DATE CREATED:                                 
-# REVISED DATE: 
+# PROGRAMMER: HIBAH SINDI
+# DATE CREATED: 23/06/2024                                
+# REVISED DATE: 23/06/2024 
 # PURPOSE: This is a *hints* file to help guide students in creating the 
 #          function adjust_results4_isadog that adjusts the results dictionary
 #          to indicate whether or not the pet image label is of-a-dog, 
